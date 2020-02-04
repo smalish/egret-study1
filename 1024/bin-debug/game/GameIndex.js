69e0ca31be93a6320c18d0335a41b41646bd1005
yangying01 <yangying01@rong360.com>
2020-02-04 16:46:17 +0800

    初始化游戏随机位置生成色块

--- 1024/bin-debug/game/GameIndex.js
+++ 1024/bin-debug/game/GameIndex.js
@@ -12,6 +12,9 @@ var GameIndex = (function (_super) {
     __extends(GameIndex, _super);
     function GameIndex() {
         var _this = _super.call(this) || this;
+        //二维数组
+        _this.len = 4; //二维数组宽高
+        _this.rect_array = [];
         // 触摸数据
         _this.touchstart_x = 0;
         _this.touchstart_y = 0;
@@ -36,32 +39,149 @@ var GameIndex = (function (_super) {
     // 初始化页面
     GameIndex.prototype.initPage = function () {
         var that = this;
+        // 游戏开始，生成随机数
+        that.initGame();
         that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.touchBegin, this);
-        // that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.touchMove, this)
+        // that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.touchMove, this)        
+        that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_RELEASE_OUTSIDE, this.touchOutside, this);
         that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_END, this.touchEnd, this);
+    };
+    GameIndex.prototype.initGame = function () {
+        var that = this;
+        // 二维数组初始化
+        for (var i = 0; i < that.len; i++) {
+            that.rect_array[i] = [];
+            for (var j = 0; j < that.len; j++) {
+                that.rect_array[i][j] = 0;
+            }
+        }
+        console.log(that.rect_array);
+        that.randNum();
+        that.randNum();
+        // 显示数组块
+        that.initNumRect();
+        console.log(that.rect_array);
+    };
+    // 在随机位置生成2或4
+    GameIndex.prototype.randNum = function () {
+        var that = this;
+        var random_x = Math.floor(Math.random() * 4); //活动【0-3】中的随机整数
+        var random_y = Math.floor(Math.random() * 4);
+        while (true) {
+            if (!that.rect_array[random_y][random_x]) {
+                break;
+            }
+            else {
+                random_x = Math.floor(Math.random() * 4); //活动【0-3】中的随机整数
+                random_y = Math.floor(Math.random() * 4);
+            }
+        }
+        that.rect_array[random_y][random_x] = Math.random() > 0.5 ? 4 : 2;
+    };
+    // 获取数字色块
+    GameIndex.prototype.getBackgroundColor = function (number) {
+        switch (number) {
+            case 2:
+                return 0xeee4da;
+                break;
+            case 4:
+                return 0xede0c8;
+                break;
+            case 8:
+                return 0xf2b179;
+                break;
+            case 16:
+                return 0xf59563;
+                break;
+            case 32:
+                return 0xf67c5f;
+                break;
+            case 64:
+                return 0xf65e3b;
+                break;
+            case 128:
+                return 0xedcf72;
+                break;
+            case 256:
+                return 0xedcc61;
+                break;
+            case 512:
+                return 0x9c0;
+                break;
+            case 1024:
+                return 0x33b5e5;
+                break;
+            case 2048:
+                return 0x09c;
+                break;
+            case 4096:
+                return 0xa6c;
+                break;
+            case 8192:
+                return 0x93c;
+                break;
+        }
+    };
+    // 初始化数字快
+    GameIndex.prototype.initNumRect = function () {
+        var that = this;
+        // that.rect_array
+        var cur_rect = null;
+        var new_shape = null;
+        for (var i = 0; i < that.len; i++) {
+            for (var j = 0; j < that.len; j++) {
+                if (that.rect_array[i][j]) {
+                    //有值的位置
+                    cur_rect = that.group_rects.getChildAt(that.len * i + j);
+                    //创建一个块
+                    new_shape = new egret.Shape();
+                    var color = that.getBackgroundColor(that.rect_array[i][j]);
+                    new_shape.graphics.beginFill(color);
+                    new_shape.graphics.drawRect(cur_rect.x, cur_rect.y, cur_rect.width, cur_rect.height);
+                    new_shape.graphics.endFill();
+                    that.group_numbers.addChild(new_shape);
+                }
+            }
+        }
     };
     // 触摸开始
     GameIndex.prototype.touchBegin = function (e) {
         var that = this;
-        console.log('touchBegin -------- x: ' + e.localX + '; y: ' + e.localY);
-        that.touchstart_x = e.localX || 0;
-        that.touchstart_y = e.localY || 0;
+        //e.localX e.localY取到的值并不是相对于group_numbers，而是相对于每个小个子的x？？？？？？？
+        console.log('touchBegin -------- x: ' + e.stageX + '; y: ' + e.stageY);
+        that.touchstart_x = e.stageX || 0;
+        that.touchstart_y = e.stageY || 0;
         // console.log(e)
     };
     // 触摸移动
-    // private touchMove(e:egret.Event): void{
+    // private touchMove(e:egret.TouchEvent): void{
     //     let that = this
-    //     console.log('touchMove: ')
-    //     console.log(e)
+    //     console.log('touchMove -------- x: ' + e.stageX + '; y: ' + e.stageY)
+    //     // console.log(e)
     // }
+    // 触摸移动到容器外
+    GameIndex.prototype.touchOutside = function (e) {
+        var that = this;
+        console.log('touchOutside -------- x: ' + e.stageX + '; y: ' + e.stageY);
+        var touchend_x = e.stageX || 0;
+        var touchend_y = e.stageY || 0;
+        var move_x = touchend_x - that.touchstart_x;
+        var move_y = touchend_y - that.touchstart_y;
+        that.judgeDirection(move_x, move_y);
+    };
     // 触摸结束
     GameIndex.prototype.touchEnd = function (e) {
         var that = this;
-        console.log('touchEnd -------- x: ' + e.localX + '; y: ' + e.localY);
-        var touchend_x = e.localX || 0;
-        var touchend_y = e.localY || 0;
+        console.log('touchEnd -------- x: ' + e.stageX + '; y: ' + e.stageY);
+        var touchend_x = e.stageX || 0;
+        var touchend_y = e.stageY || 0;
         var move_x = touchend_x - that.touchstart_x;
         var move_y = touchend_y - that.touchstart_y;
+        that.judgeDirection(move_x, move_y);
+    };
+    // 判断移动方向
+    GameIndex.prototype.judgeDirection = function (move_x, move_y) {
+        var that = this;
         if (Math.abs(move_x) >= Math.abs(move_y)) {
             // x轴上移动距离大
             if (move_x >= 0) {
@@ -88,7 +208,12 @@ var GameIndex = (function (_super) {
     //移动数字
     GameIndex.prototype.moveNumber = function (direction) {
         var that = this;
-        alert(direction);
+        console.log(direction);
+        // wx.showToast({
+        //     title: direction,
+        //     icon: 'success',
+        //     duration: 2000
+        // })
     };
     return GameIndex;
 }(eui.Component));
--- 1024/src/game/GameIndex.ts
+++ 1024/src/game/GameIndex.ts
@@ -2,7 +2,12 @@ class GameIndex extends eui.Component implements eui.UIComponent{
 
     private group_main: eui.Group
     private rect_main: eui.Rect //白色背景
+    private group_rects: eui.Group
     private group_numbers: eui.Group
+
+    //二维数组
+    private len: number = 4 //二维数组宽高
+    private rect_array: Array<any> = []
 
     // 触摸数据
     private touchstart_x: number = 0
@@ -39,41 +44,158 @@ class GameIndex extends eui.Component implements eui.UIComponent{
     private initPage():void{
         let that = this
 
+        // 游戏开始，生成随机数
+        that.initGame()
+
         that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.touchBegin, this)
-        // that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.touchMove, this)
+        // that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_MOVE, this.touchMove, this)        
+        that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_RELEASE_OUTSIDE, this.touchOutside, this)
         that.group_numbers.addEventListener(egret.TouchEvent.TOUCH_END, this.touchEnd, this)
         
+    }
+
+    private initGame():void{
+        let that = this
+
+        // 二维数组初始化
+        for(let i = 0; i < that.len; i++){
+            that.rect_array[i] = []
+            for(let j = 0; j < that.len; j++){
+                that.rect_array[i][j] = 0
+            }
+        }
+
+        
+        console.log(that.rect_array)
+
+        that.randNum()
+        that.randNum()
+
+        // 显示数组块
+        that.initNumRect()
+
+        console.log(that.rect_array)
+    }
+
+    // 在随机位置生成2或4
+    private randNum(): void{
+        let that = this
+
+        let random_x:number = Math.floor(Math.random()*4) //活动【0-3】中的随机整数
+        let random_y:number = Math.floor(Math.random()*4)
+
+        while(true){
+            if(!that.rect_array[random_y][random_x]){
+                break
+            }else{
+                random_x = Math.floor(Math.random()*4) //活动【0-3】中的随机整数
+                random_y = Math.floor(Math.random()*4)
+            }
+        }
+
+        that.rect_array[random_y][random_x] = Math.random() > 0.5 ? 4 : 2
+        
+    }
+
+    // 获取数字色块
+    private getBackgroundColor(number: number): number{
+        switch (number) {
+            case 2:return 0xeee4da;break;
+            case 4:return 0xede0c8;break;
+            case 8:return 0xf2b179;break;
+            case 16:return 0xf59563;break;
+            case 32:return 0xf67c5f;break;
+            case 64:return 0xf65e3b;break;
+            case 128:return 0xedcf72;break;
+            case 256:return 0xedcc61;break;
+            case 512:return 0x9c0;break;
+            case 1024:return 0x33b5e5;break;
+            case 2048:return 0x09c;break;
+            case 4096:return 0xa6c;break;
+            case 8192:return 0x93c;break;
+        }
+    }
+
+    // 初始化数字快
+    private initNumRect():void{
+        let that = this
+
+        // that.rect_array
+        let cur_rect: any = null
+        let new_shape: egret.Shape = null
+        for(let i = 0; i < that.len; i++){
+            for(let j = 0; j < that.len; j++){
+                if(that.rect_array[i][j]){
+                    //有值的位置
+                    cur_rect = that.group_rects.getChildAt(that.len*i + j)
+                    
+                    //创建一个块
+                    new_shape = new egret.Shape();
+                    var color: number = that.getBackgroundColor(that.rect_array[i][j])
+                    new_shape.graphics.beginFill(color);
+                    new_shape.graphics.drawRect(cur_rect.x, cur_rect.y, cur_rect.width, cur_rect.height)
+                    new_shape.graphics.endFill();
+                    that.group_numbers.addChild(new_shape);
+
+                    
+                }
+            }
+        }
+
     }
 
     // 触摸开始
     private touchBegin(e:egret.TouchEvent): void{
         let that = this
 
-        console.log('touchBegin -------- x: ' + e.localX + '; y: ' + e.localY)
-        that.touchstart_x = e.localX || 0
-        that.touchstart_y = e.localY || 0
+        //e.localX e.localY取到的值并不是相对于group_numbers，而是相对于每个小个子的x？？？？？？？
+
+        console.log('touchBegin -------- x: ' + e.stageX + '; y: ' + e.stageY)
+        that.touchstart_x = e.stageX || 0
+        that.touchstart_y = e.stageY || 0
         // console.log(e)
     }
 
     // 触摸移动
-    // private touchMove(e:egret.Event): void{
+    // private touchMove(e:egret.TouchEvent): void{
     //     let that = this
 
 
-    //     console.log('touchMove: ')
-    //     console.log(e)
+    //     console.log('touchMove -------- x: ' + e.stageX + '; y: ' + e.stageY)
+    //     // console.log(e)
     // }
+
+    // 触摸移动到容器外
+    private touchOutside(e: egret.TouchEvent):void{
+        let that = this
+
+        console.log('touchOutside -------- x: ' + e.stageX + '; y: ' + e.stageY)
+        let touchend_x:number = e.stageX || 0
+        let touchend_y:number = e.stageY || 0
+
+        let move_x:number = touchend_x - that.touchstart_x
+        let move_y:number = touchend_y - that.touchstart_y
+
+        that.judgeDirection(move_x, move_y)
+    }
  
     // 触摸结束
     private touchEnd(e: egret.TouchEvent):void{
         let that = this
 
-        console.log('touchEnd -------- x: ' + e.localX + '; y: ' + e.localY)
-        let touchend_x:number = e.localX || 0
-        let touchend_y:number = e.localY || 0
+        console.log('touchEnd -------- x: ' + e.stageX + '; y: ' + e.stageY)
+        let touchend_x:number = e.stageX || 0
+        let touchend_y:number = e.stageY || 0
 
         let move_x:number = touchend_x - that.touchstart_x
         let move_y:number = touchend_y - that.touchstart_y
+
+        that.judgeDirection(move_x, move_y)
+    }
+
+    // 判断移动方向
+    private judgeDirection(move_x: number, move_y: number): void{
+        let that = this
 
         if(Math.abs(move_x) >= Math.abs(move_y)){
             // x轴上移动距离大
@@ -101,7 +223,12 @@ class GameIndex extends eui.Component implements eui.UIComponent{
        let that = this
 
 
-      alert(direction)
+      console.log(direction)
+    // wx.showToast({
+    //     title: direction,
+    //     icon: 'success',
+    //     duration: 2000
+    // })
 
     }
 
